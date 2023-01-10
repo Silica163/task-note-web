021c62001a6ae6fd791e73ee631f41d297e533cf
Silica163 <96188282+Silica163@users.noreply.github.com>
2023-01-10 18:28:02 +0700

    set post.php to accept only json post

--- api/post.php
+++ api/post.php
@@ -1,5 +1,19 @@
 <?php
-
-
-
+function check_request(){
+	if($_SERVER["REQUEST_METHOD"] != "POST")return false;
+	if(getallheaders()["Content-type"] != "application/json")return false;
+	return true;
+}
+if(!check_request()){
+	http_response_code(400);
+	exit();
+}
+echo "<pre>";
+echo $_SERVER["REQUEST_METHOD"]." ".$_SERVER["REQUEST_URI"]." ".$_SERVER["SERVER_PROTOCOL"]."\n";
+foreach (getallheaders() as $name => $value) {
+	echo "$name: $value \n";
+}
+echo file_get_contents("php://input");
+// 400: $text = 'Bad Request'
+echo "</pre>";
 ?>
